0fe1f62b13c738141fd9e9e22edcf90c58b06762
tobilasinde <tobilasinde2012@yahoo.com>
2024-10-29 22:25:48 -0400

    initial

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // to point to correct path for gh-pages
-  base: 'sendbird-supportchat-sample-react',
-  server: {
-    port: 3000,
-  },
+	plugins: [react()],
+	// to point to correct path for gh-pages
+	base: '/',
+	server: {
+		port: 3000,
+	},
 })
