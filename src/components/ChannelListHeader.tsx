6b4fae4fe0474403c385634a618e60584ce2e6e3
tobilasinde <tobilasinde2012@yahoo.com>
2024-10-31 13:45:03 -0400

    remove salesforce integration

--- src/components/ChannelListHeader.tsx
+++ src/components/ChannelListHeader.tsx
@@ -1,17 +1,12 @@
 import { useRef, useState } from 'react'
-
 import useSendbirdStateContext from '@sendbird/uikit-react/useSendbirdStateContext'
 import sendbirdSelectors from '@sendbird/uikit-react/sendbirdSelectors'
 import {
-	SALESFORCE_API_URL,
 	USER_ID,
 	SALESFORCE_SUPPORT_CHAT_CHANNEL,
 	CHANNEL_COVER_IMAGE,
-	SALESFORCE_CLIENT_ID,
-	SALESFORCE_CLIENT_SECRET,
 } from '../consts'
 import { getRandomChannelName } from '../utils'
-import axios from 'axios'
 
 export default function ChannelListHeader() {
 	const [loading, setLoading] = useState(false)
@@ -28,38 +23,12 @@ export default function ChannelListHeader() {
 					try {
 						setLoading(true)
 						const title = getRandomChannelName()
-						const channel = await createGroupChannel({
+						await createGroupChannel({
 							customType: SALESFORCE_SUPPORT_CHAT_CHANNEL,
 							invitedUserIds: [USER_ID],
 							name: title,
 							coverUrl: CHANNEL_COVER_IMAGE,
 						})
-						const auth = await axios.post(
-							`${SALESFORCE_API_URL}/services/oauth2/token?grant_type=client_credentials&client_id=${SALESFORCE_CLIENT_ID}&client_secret=${SALESFORCE_CLIENT_SECRET}`,
-							{},
-							{
-								headers: {
-									'Referrer-Policy': 'no-referrer',
-								},
-							}
-						)
-						await axios.post(
-							`${SALESFORCE_API_URL}/services/data/v42.0/sobjects/Case/`,
-							{
-								Subject: title,
-								Description: 'This is a test case',
-								Sendbird__UserId__c: 'test-user',
-								Sendbird__ChannelUrl__c: channel.url,
-								Sendbird__IsEinsteinBotsCase__c: false,
-							},
-							{
-								headers: {
-									Accept: 'application/json',
-									'Referrer-Policy': 'no-referrer',
-									Authorization: `Bearer ${auth.data.access_token}`,
-								},
-							}
-						)
 					} catch (error) {
 						console.error('Errorsss:', error)
 					} finally {
--- src/consts.ts
+++ src/consts.ts
@@ -3,12 +3,7 @@
 export const APP_ID = import.meta.env.VITE_APP_ID ?? ''
 export const USER_ID = import.meta.env.VITE_USER_ID ?? ''
 export const NICKNAME = import.meta.env.VITE_NICK_NAME ?? ''
-export const SALESFORCE_API_URL = import.meta.env.VITE_SALESFORCE_API_URL ?? ''
 export const CHANNEL_COVER_IMAGE =
 	import.meta.env.VITE_CHANNEL_COVER_IMAGE ?? ''
-export const SALESFORCE_CLIENT_ID =
-	import.meta.env.VITE_SALESFORCE_CLIENT_ID ?? ''
-export const SALESFORCE_CLIENT_SECRET =
-	import.meta.env.VITE_SALESFORCE_CLIENT_SECRET ?? ''
 
 export const SALESFORCE_SUPPORT_CHAT_CHANNEL = 'SALESFORCE_SUPPORT_CHAT_CHANNEL'
